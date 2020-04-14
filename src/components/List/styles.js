4edfdba4a9ab64e87320f7dd5d890f11dc1eed55
Vitor <vitorrubim1@hotmail.com>
2020-04-14 15:57:01 -0300

    Fazendo com que os cards concluídos tenham uma opacidade

--- src/components/List/styles.js
+++ src/components/List/styles.js
@@ -4,6 +4,8 @@ export const Container = styled.div`
     padding: 0 15px;
     height: 100%;
     flex: 0 0 320px;
+    opacity: ${props => props.done ? 0.6 : 1} ;
+    /*IF INLINE, CASO O CASO JÁ TENHA SIDO ENCERRADO, DA OPACIDADE*/
 
     & + div{
         border-left: 1px solid rgba(0, 0, 0, 0.5);
