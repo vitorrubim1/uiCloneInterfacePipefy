8b4b9871cafbfb6108a3eca9c550b01f1497bc7a
Vitor <vitorrubim1@hotmail.com>
2020-04-15 13:24:07 -0300

    Ajustes no css

--- src/services/api.js
+++ src/services/api.js
@@ -8,31 +8,31 @@ export function loadLists() {
             id: 1,
             content: 'Estudar módulo 01 de NodeJS',
             labels: ['#7159c1'],
-            user: 'https://avatars0.githubusercontent.com/u/48107882?s=460&u=8c3f30bced3d09a3158b9da5bbbbd154f121da26&v=4'
+            user: 'https://www.linkedin.com/dms/C4E04AQFUBzYSNY2Z4A/profile-originalphoto-shrink_900_1200/0?m=AQKL4gSmwB5epwAAAXF-dm4G4DPLQqm4Xj6SnCcRS6hJ_BscW8G2SnZtbg&e=1587051022&v=beta&t=JIU0ZW6_hPD6Dn67dWQveRwl9Q4WNuLuuzjeNJlbb9Q'
           },
           {
             id: 2,
             content: 'Criar vídeo para o Youtube ensinando a recriar a interface do Pipefy',
             labels: ['#7159c1'],
-            user: 'https://avatars0.githubusercontent.com/u/48107882?s=460&u=8c3f30bced3d09a3158b9da5bbbbd154f121da26&v=4'
+            user: 'https://www.linkedin.com/dms/C4E04AQFUBzYSNY2Z4A/profile-originalphoto-shrink_900_1200/0?m=AQKL4gSmwB5epwAAAXF-dm4G4DPLQqm4Xj6SnCcRS6hJ_BscW8G2SnZtbg&e=1587051022&v=beta&t=JIU0ZW6_hPD6Dn67dWQveRwl9Q4WNuLuuzjeNJlbb9Q'
           },
           {
             id: 3,
             content: 'Estudar módulo 03 de React Native',
             labels: ['#7159c1'],
-            user: 'https://avatars0.githubusercontent.com/u/48107882?s=460&u=8c3f30bced3d09a3158b9da5bbbbd154f121da26&v=4'
+            user: 'https://www.linkedin.com/dms/C4E04AQFUBzYSNY2Z4A/profile-originalphoto-shrink_900_1200/0?m=AQKL4gSmwB5epwAAAXF-dm4G4DPLQqm4Xj6SnCcRS6hJ_BscW8G2SnZtbg&e=1587051022&v=beta&t=JIU0ZW6_hPD6Dn67dWQveRwl9Q4WNuLuuzjeNJlbb9Q'
           },
           {
             id: 4,
             content: 'Gravar Aula "NextJS: Utilizando server-side rendering com ReactJS"',
             labels: ['#54e1f7'],
-            user: 'https://avatars0.githubusercontent.com/u/48107882?s=460&u=8c3f30bced3d09a3158b9da5bbbbd154f121da26&v=4'
+            user: 'https://www.linkedin.com/dms/C4E04AQFUBzYSNY2Z4A/profile-originalphoto-shrink_900_1200/0?m=AQKL4gSmwB5epwAAAXF-dm4G4DPLQqm4Xj6SnCcRS6hJ_BscW8G2SnZtbg&e=1587051022&v=beta&t=JIU0ZW6_hPD6Dn67dWQveRwl9Q4WNuLuuzjeNJlbb9Q'
           },
           {
             id: 5,
             content: 'Gravar testes e deploy ReactJS',
             labels: ['#54e1f7'],
-            user: 'https://avatars0.githubusercontent.com/u/48107882?s=460&u=8c3f30bced3d09a3158b9da5bbbbd154f121da26&v=4'
+            user: 'https://www.linkedin.com/dms/C4E04AQFUBzYSNY2Z4A/profile-originalphoto-shrink_900_1200/0?m=AQKL4gSmwB5epwAAAXF-dm4G4DPLQqm4Xj6SnCcRS6hJ_BscW8G2SnZtbg&e=1587051022&v=beta&t=JIU0ZW6_hPD6Dn67dWQveRwl9Q4WNuLuuzjeNJlbb9Q'
           },
         ]
       },
@@ -44,7 +44,7 @@ export function loadLists() {
             id: 6,
             content: 'Recriando clone do Pipefy',
             labels: [],
-            user: 'https://avatars0.githubusercontent.com/u/48107882?s=460&u=8c3f30bced3d09a3158b9da5bbbbd154f121da26&v=4'
+            user: 'https://www.linkedin.com/dms/C4E04AQFUBzYSNY2Z4A/profile-originalphoto-shrink_900_1200/0?m=AQKL4gSmwB5epwAAAXF-dm4G4DPLQqm4Xj6SnCcRS6hJ_BscW8G2SnZtbg&e=1587051022&v=beta&t=JIU0ZW6_hPD6Dn67dWQveRwl9Q4WNuLuuzjeNJlbb9Q'
           }
         ]
       },
@@ -56,13 +56,13 @@ export function loadLists() {
             id: 7,
             content: 'Gravar sobre Geolocalização e mapas com React Native',
             labels: ['#7159c1'],
-            user: 'https://avatars0.githubusercontent.com/u/48107882?s=460&u=8c3f30bced3d09a3158b9da5bbbbd154f121da26&v=4'
+            user: 'https://www.linkedin.com/dms/C4E04AQFUBzYSNY2Z4A/profile-originalphoto-shrink_900_1200/0?m=AQKL4gSmwB5epwAAAXF-dm4G4DPLQqm4Xj6SnCcRS6hJ_BscW8G2SnZtbg&e=1587051022&v=beta&t=JIU0ZW6_hPD6Dn67dWQveRwl9Q4WNuLuuzjeNJlbb9Q'
           },
           {
             id: 8,
             content: 'Gravar testes e deploy ReactJS',
             labels: ['#54e1f7'],
-            user: 'https://avatars0.githubusercontent.com/u/48107882?s=460&u=8c3f30bced3d09a3158b9da5bbbbd154f121da26&v=4'
+            user: 'https://www.linkedin.com/dms/C4E04AQFUBzYSNY2Z4A/profile-originalphoto-shrink_900_1200/0?m=AQKL4gSmwB5epwAAAXF-dm4G4DPLQqm4Xj6SnCcRS6hJ_BscW8G2SnZtbg&e=1587051022&v=beta&t=JIU0ZW6_hPD6Dn67dWQveRwl9Q4WNuLuuzjeNJlbb9Q'
           },
           {
             id: 9,
